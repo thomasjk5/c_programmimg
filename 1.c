8917115ca87fee8ae48f104c8680fd8e041a816f
thomasjk5 <43922701+thomasjk5@users.noreply.github.com>
2019-04-30 09:00:11 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int even (int a)
+ { if(a%2==0)
+     {
+      return 1;
+     }
+    else
+     {
+      return 0;
+     }
+  
+  }
+void main()
+{
+  int arr[10],i,n;
+  printf("Enter the number of elements " );
+  scanf("%d",&n);
+  printf("Enter the elements : ");
+  for(i=0;i<n;i++)
+   { scanf("%d",&arr[i]);
+   }
+  x=even(arr[1]);
+  if(x==1}
+  { 
+   printf("%d is Even",arr[1]");
+  }  
+  else
+  {
+   printf("%d is Odd",arr[1]");
+  }
+}
+
